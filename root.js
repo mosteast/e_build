28b20a22696b5393588a2077c897e03e3402d246
kev <kev@localhost>
2020-01-10 18:17:34 +0800

    Auto commit: 2020-01-10_18:17:33

--- root.js
+++ root.js
@@ -4,27 +4,16 @@
  |--------------------------------------------------------------------------
  | !!! THIS FILE IS LOCATION RESTRICTED, DON'T MOVE IT !!!
  */
-var __spreadArrays = (this && this.__spreadArrays) || function () {
-    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
-    for (var r = Array(s), k = 0, i = 0; i < il; i++)
-        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
-            r[k] = a[j];
-    return r;
-};
-var resolve = require('path').resolve;
-var stat = require('fs').stat;
+const { resolve } = require('path');
+const { stat } = require('fs');
 /**
  * App base path
  * @param segments
  *
  * @example Get app base dir
  */
-function dir_root() {
-    var segments = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        segments[_i] = arguments[_i];
-    }
-    return resolve.apply(void 0, __spreadArrays([__dirname], segments));
+function dir_root(...segments) {
+    return resolve(__dirname, ...segments);
 }
-module.exports = { dir_root: dir_root };
+module.exports = { dir_root };
 //# sourceMappingURL=root.js.map
--- src/e.js
+++ src/e.js
@@ -1,43 +1,20 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 Object.defineProperty(exports, "__esModule", { value: true });
-var make_error_1 = require("make-error");
-var E = /** @class */ (function (_super) {
-    __extends(E, _super);
-    function E() {
-        var argS = [];
-        for (var _i = 0; _i < arguments.length; _i++) {
-            argS[_i] = arguments[_i];
-        }
-        var _this = _super.call(this) || this;
+const make_error_1 = require("make-error");
+class E extends make_error_1.BaseError {
+    constructor(...argS) {
+        super();
         /**
          * Error chain from inheritance.
          * @example [ 'e', 'external', 'invalid_api_argument' ]
          */
-        _this.chain = [];
-        _this.init.apply(_this, argS);
-        return _this;
+        this.chain = [];
+        this.init(...argS);
         // this.name = this.constructor.name
         // Error.captureStackTrace(this, this.constructor)
     }
-    E.prototype.init = function () {
-        var a = [];
-        for (var _i = 0; _i < arguments.length; _i++) {
-            a[_i] = arguments[_i];
-        }
-        var a0 = a[0];
+    init(...a) {
+        const a0 = a[0];
         switch (a.length) {
             case 1:
                 switch (typeof a0) {
@@ -50,7 +27,7 @@ var E = /** @class */ (function (_super) {
                 }
                 break;
             case 2:
-                var a1 = a[1];
+                let a1 = a[1];
                 this.message = a0;
                 switch (typeof a1) {
                     case 'string':
@@ -65,46 +42,44 @@ var E = /** @class */ (function (_super) {
         // this.stack = (new Error()).stack
         this.generate_chain();
         this.generate_echain();
-    };
+    }
     /**
      * Generate error chain based on inheritance
      * @param ins
      * @param echain
      */
-    E.prototype.generate_chain = function (ins) {
-        if (ins === void 0) { ins = undefined; }
+    generate_chain(ins = undefined) {
         if (ins === undefined) {
             ins = this;
         }
         ins = Object.getPrototypeOf(ins);
-        var name = ins.constructor.name.toLowerCase();
+        const name = ins.constructor.name.toLowerCase();
         this.chain.unshift(name);
         if (ins.constructor === E || !ins.constructor) {
             return;
         }
         return this.generate_chain(ins);
-    };
+    }
     /**
      * Generate and get echain string
      * @returns {string}
      */
-    E.prototype.generate_echain = function () {
+    generate_echain() {
         if (!this.echain) {
             this.echain = this.chain.join('.');
         }
         return this.echain;
-    };
+    }
     /**
      * Fill `this` with a another object
      * @param obj
      */
-    E.prototype.fill = function (obj) {
-        for (var key in obj) {
+    fill(obj) {
+        for (let key in obj) {
             this[key] = obj[key];
         }
-    };
-    return E;
-}(make_error_1.BaseError));
+    }
+}
 exports.E = E;
 // // @ts-ignore
 // E.prototype = new Error
